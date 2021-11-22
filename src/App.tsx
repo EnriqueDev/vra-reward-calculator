1fdba37ecb0fe94ad151232a94f5b65258607eec
Enrique Martin <enrique.martin@aboutyou.com>
2021-11-22 01:22:58 +0100

    feat!: implemented routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 import * as React from 'react'
-import { CssBaseline } from '@mui/material'
-import { AppBar, DataGrid } from 'components'
+import { CssBaseline, Switch } from '@mui/material'
+import { AppBar } from 'components'
 import { data } from './fixtures/api'
 import { baseTransformer } from 'utils/transformers/baseTransformer'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Auth from 'modules/Auth/Auth'
+import { TokenContextProvider } from 'context/token'
 
 const App: React.FC = () => {
   const transactions = React.useMemo(() => baseTransformer(data), [])
 
   return (
     <React.StrictMode>
-      <CssBaseline />
-      <AppBar />
-      <DataGrid data={transactions} />
+      <TokenContextProvider>
+        <CssBaseline />
+        <AppBar />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Auth />} />
+          </Routes>
+        </BrowserRouter>
+      </TokenContextProvider>
     </React.StrictMode>
   )
 }
